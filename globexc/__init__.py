5ae8d3e24ce39533cd2978643a4161b0450bbf93
Mike Elson <mike.elson@gmail.com>
2013-11-11 01:39:58 +0100

    bump version number

--- globexc/__init__.py
+++ globexc/__init__.py
@@ -7,7 +7,7 @@ message and also print a full stack trace including local variable contents.
 from __future__ import print_function
 from __future__ import unicode_literals
 
-__version__ = '0.1'
+__version__ = '0.1.0'
 
 import os
 import sys
